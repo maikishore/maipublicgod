853300110fa84425edd1c437266a6d4cbe5718ce
maibrain <maibrainceo@gmail.com>
2021-07-30 13:32:28 +0530

    Login Form UI/UX Done

--- src/test.js
+++ src/test.js
@@ -1,8 +1,7 @@
 import React from "react";
-import login_illustration from "./Assets/loginsidegirl.png"
+import login_illustration from "./Assets/loginsidegirl.png";
 
-import maibrain_logo from "./Assets/maibrain_logo.png"
-
+import maibrain_logo from "./Assets/maibrain_logo.png";
 
 const styles = {
   body: "h-screen w-full bg-white-400 ",
@@ -14,63 +13,71 @@ const styles = {
 export default function Test() {
   const classes = styles;
   return (
-      <div>
-  <div class="flex justify-start">
-  <div class=" p-2 avatar">
-  <div class="mt-2 w-28 h-28 ">
-    <img alt="Maibrain" src={maibrain_logo} />
-  </div>
-</div> 
-  </div> 
+    <div className="">
+      <Logo src={maibrain_logo} />
 
-
-
-
-
-
-
- <div class="hero min-h-screen  bg-white-200">
-       
-       <div class="flex-col justify-evenly hero-content ">
-         <div class="max-w-md">
-             <img alt="" src={login_illustration} />
-         </div>
-         <div class="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
-           <div class="card-body w-150  ">
-             <div class="form-control">
-               <label class="label">
-                 <span class="label-text">Email</span>
-               </label>
-               <input
-                 type="text"
-                 placeholder="email"
-                 class="input input-bordered"
-               />
-             </div>
-             <div class="form-control">
-               <label class="label">
-                 <span class="label-text">Password</span>
-               </label>
-               <input
-                 type="text"
-                 placeholder="password"
-                 class="input input-bordered"
-               />
-               <label class="label">
-                 <a href="#" class="label-text-alt">
-                   Forgot password?
-                 </a>
-               </label>
-             </div>
-             <div class="form-control mt-6">
-               <input type="button" value="Login" class="btn btn-primary" />
-             </div>
-           </div>
-         </div>
-       </div>
-     </div>
-
-      </div>
-   
+      <LoginForm />
+    </div>
   );
 }
+
+
+
+function LoginForm() {
+  return (<div className="flex flex-nowrap my-8 lg:items-center lg:flex-row  sm:flex-col justify-center sm:items-center ">
+  <div class="md:w-3/5 sm:w-2/3 ">
+    <img alt="" src={login_illustration} />
+  </div>
+  <div class="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
+    <div class="card-body  ">
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text">Email</span>
+        </label>
+        <input
+          type="email"
+          placeholder="email"
+          class="input input-bordered font-semibold"
+        />
+      </div>
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text">Password</span>
+        </label>
+        <input
+          type="password"
+          placeholder="password"
+          class="input input-bordered font-semibold"
+        />
+        <label class="label">
+          <a href="#" class="label-text-alt font-semibold">
+            Forgot password?
+          </a>
+        </label>
+      </div>
+      <div class="form-control mt-6">
+        <input type="button" value="Login" class="btn btn-primary" />
+      </div>
+    </div>
+  </div>
+</div>)
+
+}
+
+function Logo(props) {
+  return (
+    <div class="flex justify-start">
+      <div class=" p-2 avatar">
+        <div class="mt-2 w-28 h-28 ">
+          <img alt="Maibrain" src={props.src} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*
+
+     
+         
+*/
