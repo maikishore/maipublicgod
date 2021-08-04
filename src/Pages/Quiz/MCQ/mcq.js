0ac3e597492c5aa830fa5a1fc4c7cdc1a50347af
maibrain <maibrainceo@gmail.com>
2021-08-04 18:51:48 +0530

    Checkpoint 101 mcq before editing

--- src/Pages/Quiz/MCQ/mcq.js
+++ src/Pages/Quiz/MCQ/mcq.js
@@ -1,118 +1,193 @@
-import React from 'react'
-import Navbar from '../../../Commons/Navbar/Navbar';
-import McqCard from './mcqcard';
-
+import React from "react";
+import Navbar from "../../../Commons/Navbar/Navbar";
+import McqCard from "./mcqcard";
 
 function Mcqq() {
-    const data = [
-        {
-          question: "What is your 1 name",
-          options: ["ashf", "sak;fs", "aksfdj", "askh"],
-          correctanswer:1  
-        },
+  const data = [
+    {
+      question: "What is your 1 name",
+      options: ["ashf", "sak;fs", "aksfdj", "askh"],
+      correctanswer: 1,
+    },
 
-        {
-            question: "What is your 2 name",
-            options: ["ashdsgf", "sak##;fs", "akssfdj", "askh"],
-            correctanswer:2  
-          },
+    {
+      question: "What is your 2 name",
+      options: ["ashdsgf", "sak##;fs", "akssfdj", "askh"],
+      correctanswer: 2,
+    },
 
+    {
+      question: "What is your 3 name",
+      options: ["ashsf", "sasafk;fs", "aks121fdj", "askh"],
+      correctanswer: 3,
+    },
 
-          {
-            question: "What is your 3 name",
-            options: ["ashsf", "sasafk;fs", "aks121fdj", "askh"],
-            correctanswer:3 
-          },
+    {
+      question: "What is your 4 name",
+      options: ["ash##f", "sak;fs", "aks##fdj", "askh"],
+      correctanswer: 4,
+    },
+  ];
 
-          {
-            question: "What is your 4 name",
-            options: ["ash##f", "sak;fs", "aks##fdj", "askh"],
-            correctanswer:4 
-          },
-      ];
-
-    return (
-        <div>
-            <Navbar />
-<McqCard data={data}  question={data[0].question}/>
-
-        </div>
-    )
+  return (
+    <div>
+      <Navbar />
+      <McqCard data={data} question={data[0].question} />
+    </div>
+  );
 }
 
+function correct(arr1,arr2) {
+console.log(arr1,arr2,"called")
+  var s=0
+  for(var i=0;i<arr1.length;i++){
+    if(arr1[i].toString()===arr2[i].toString()){
+      s=s+1
+    } 
+  }
+  console.log("sum",s)
+  return s
+}
+
+
+
 function Mcq() {
-    const data = [
-        {
-          question: "What is your 1 name",
-          options: ["ashf", "sak;fs", "aksfdj", "askh"],
-          correctanswer:1  
-        },
+  const [qnumber,setQNumber]=React.useState([])
+  const [cnumber,setCNumber]=React.useState([])
 
-        {
-            question: "What is your 2 name",
-            options: ["ashdsgf", "sak##;fs", "akssfdj", "askh"],
-            correctanswer:2  
-          },
+ 
+  const [currentIndex,setCurrentIndex]=React.useState(0)
+ 
+  var correctanswers=[]
 
 
-          {
-            question: "What is your 3 name",
-            options: ["ashsf", "sasafk;fs", "aks121fdj", "askh"],
-            correctanswer:3 
-          },
 
-          {
-            question: "What is your 4 name",
-            options: ["ash##f", "sak;fs", "aks##fdj", "askh"],
-            correctanswer:4 
-          },
-      ];
 
-  
-    const mcards = data.map((each, index) => {
-      return (
-        <div
-          id={"item" + index.toString()}
-          className="flex items-center justify-center w-full text-center shadow carousel-item"
-        >
-        <McqCard data={each}  question={data[index].question}/>
-        </div>
-      );
-    });
-  
-    const mnavigation = data.map((each, index) => {
-      return (
-        <a href={`/mcq#item${index.toString()}`} className="btn btn-xs btn-circle">
-          {(index + 1).toString()}
-        </a>
-      );
-    });
-  
+
+ const data = [
+    {
+      question: "What is your 1 name",
+      options: ["ashf", "sak;fs", "aksfdj", "askh"],
+      correctanswer: 0,
+    },
+
+    {
+      question: "What is your 2 name",
+      options: ["ashdsgf", "sak##;fs", "akssfdj", "askh"],
+      correctanswer: 1,
+    },
+
+    {
+      question: "What is your 3 name",
+      options: ["ashsf", "sasafk;fs", "aks121fdj", "askh"],
+      correctanswer: 2,
+    },
+
+    {
+      question: "What is your 4 name",
+      options: ["ash##f", "sak;fs", "aks##fdj", "askh"],
+      correctanswer: 3,
+    },
+  ];
+
+
+  for(var i=0;i<data.length;i++) {
+   
+    correctanswers.push(data[i].correctanswer)
+    console.log("cca",correctanswers)
+  }
+
+
+
+React.useEffect(()=>{
+  let qnum=[]
+  for(var i=0;i<data.length;i++) {
+    qnum.push(500)
+    correctanswers.push(data[i].correctanswer)
+  }
+  //console.log("correctanswers",correctanswers)
+  setQNumber([...qnum])
+  setCNumber([...qnum])
+ ////console.log("-->",qnumber)
+
+
+
+},[])
+
+ 
+React.useEffect(()=>{
+
+return ()=>{}
+},[currentIndex])
+
+
+
+
+  const mcards = data.map((each, index) => {
     return (
-      <div className=" ">
-        <Navbar />
-        <div className="bg-red-400"></div>
-        <div
-          style={{ height: "80vh" }}
-          className="w-full   bg-gray-200 text-center carousel"
-        >
-          {mcards}
-        </div>
-  
-        <div className="flex justify-center w-full my-1 space-x-2">
-          {mnavigation}
-        </div>
+      <div
+        id={"item" + index.toString()}
+        className="flex items-center justify-center w-full text-center shadow carousel-item"
+      >
+        <McqCard data={each} question={
+          data[index].question} func={(e)=>{
+         console.log(e)
+            /*
+            let x=qnumber
+    
+            x[index]=100
+            setQNumber(prevState=>x)
+
+            let y=cnumber
+        
+            y[index]=e.currentTarget
+            setCNumber(prevState=>y)
+
+
+
+
+          
+            if(parseInt(qnumber.reduce((a, b) => a + b, 0))===parseInt(data.length*100)){
+              console.log("Hurruy!")
+             
+              console.log("correcty",correct(correctanswers,cnumber))
+            }*/
+     
+         }
+      } />
+     
       </div>
     );
-  }
-  
+  });
+
+  const mnavigation = data.map((each, index) => {
+    return (
+      <a
+        href={`/mcq#item${index.toString()}`}
+        className="btn btn-xs btn-circle"
+      >
+        {(index + 1).toString()}
+      </a>
+    );
+  });
 
 
+  return (
+    <div className=" ">
+      <Navbar />
+      <div className=""></div>
+      <div
+        style={{ height: "80vh" }}
+        className="w-full   bg-gray-200 text-center carousel"
+      >
+        {mcards}
+      </div>
 
-
-
-
-
-
+      <div className="flex justify-center w-full my-1 space-x-2">
+        {mnavigation}
+      </div>
+    </div>
+  );
+}
 
 export default Mcq;
--- src/Pages/Quiz/MCQ/mcqcard.js
+++ src/Pages/Quiz/MCQ/mcqcard.js
@@ -1,51 +1,41 @@
-import React from 'react'
-import OptionCard from './components/optioncard'
-
+import React from "react";
+import OptionCard from "./components/optioncard";
 
 function McqCard(props) {
-
-
-    const [optnum ,setOptNum]=React.useState(100)
-  
-  
-    const options=props.data.options.map((e,index)=>{
-  
-       return (<OptionCard 
-        key={index}
-       onClick={()=>{
-         console.log(index)
-         setOptNum(index)
-       }}
-       color={index===optnum?"bg-blue-500":"bg-white"}
-        optionnumber={index} option={e} />)
-     })
- 
-  
-  
-  
-  
-  
+  const [optnum, setOptNum] = React.useState(100);
+  const func=props.func
+  const options = props.data.options.map((e, index) => {
     return (
-      <div className="w-full  ">
+      <OptionCard
+        key={index}
+        value={index}
+        onClick={(e) => {
+          func()
+          setOptNum(index);
+          console.log(index)
+        }}
         
-        <div className="w-full -mt-2 h-screen bg-gray-300">
-          <div className="flex flex-row justify-center items-start">
-          
-  
-          
-            <div className="m-6 mt-40 w-1/2 h-96 shadow-md bg-white rounded-box ">
-              <div className="card m-1 text-lg text-bold text-left p-4  border-2 shadow border-gray-100 bg-white-400  rounded-box">
-        {props.question}
-              </div>
-  
-       {options}
-  
-            
-            
+        color={index === optnum ? "bg-blue-500" : "bg-white"}
+        optionnumber={String.fromCharCode(65+index)}
+        option={e}
+      />
+    );
+  });
+
+  return (
+    <div className="w-full  ">
+      <div className="w-full -mt-2 h-screen bg-gray-300">
+        <div className="flex flex-row justify-center items-start">
+          <div className="m-6 mt-40 w-1/2 h-96 shadow-md bg-white rounded-box ">
+            <div className="card m-1 text-lg text-bold text-left p-4  border-2 shadow border-gray-100 bg-white-400  rounded-box">
+              {props.question}
             </div>
+
+            {options}
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 export default McqCard;
