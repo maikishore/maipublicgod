5d2cd48bae9905b1b71264467efacbc84d573a9a
maibrain <maibrainceo@gmail.com>
2021-08-06 20:47:43 +0530

    Editor Icons test

--- src/test.js
+++ src/test.js
@@ -2,29 +2,44 @@ import React from "react";
 import Navbar from "./Commons/Navbar/Navbar";
 
 import { IoAddCircleSharp } from "react-icons/io5";
-import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 import { defaultExtensions } from "@tiptap/starter-kit";
+import BulletList from "@tiptap/extension-bullet-list";
+import "./test.css";
+import { FaBold, FaItalic, FaCode } from "react-icons/fa";
+import { MdStrikethroughS } from "react-icons/md";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import OrderedList from "@tiptap/extension-ordered-list";
+import ListItem from "@tiptap/extension-list-item";
+import Subscript from '@tiptap/extension-subscript'
 
-import "./test.css"
+import Superscript from '@tiptap/extension-superscript'
+import Image from '@tiptap/extension-image'
+import Dropcursor from '@tiptap/extension-dropcursor'
+import TextAlign from '@tiptap/extension-text-align'
 
+const styles = {
+  iconactive: "mx-1 px-2 py-2  bg-gray-400 shadow btn btn-ghost",
+  icondeactive: "mx-1 px-2 py-2 bg-gray-100 shadow btn btn-ghost",
+  ol: "list-inside",
+};
 
 function Nodes(props) {
-  const data = ["Science", "Physics", "Title","Physics"];
+  const data = ["Science", "Physics", "Title", "Physics"];
   const [nlist, setNlist] = React.useState(data);
-  const [nodeCheck,setNodecheck]=React.useState(true)
+  const [nodeCheck, setNodecheck] = React.useState(true);
 
-  const nodeRef=React.useRef()
+  const nodeRef = React.useRef();
 
-React.useEffect(()=>{
-
-  if(nodeRef.current.value.length !==0){
-   
-    setNlist(prevstate=>[...prevstate,nodeRef.current.value])
-    console.log(nlist)
-  }
-
-},[nodeCheck])
+  React.useEffect(() => {
+    if (nodeRef.current.value.length !== 0) {
+      setNlist((prevstate) => [...prevstate, nodeRef.current.value]);
+      console.log(nlist);
+    }
+  }, [nodeCheck]);
 
   const nlists = nlist.map((each, index) => {
     return (
@@ -69,16 +84,20 @@ React.useEffect(()=>{
           <li>
             <div className="form-control">
               <input
-              ref={nodeRef}
+                ref={nodeRef}
                 type="text"
                 placeholder="Node Title"
                 className="mb-1 input input-bordered"
               />
             </div>
-            <button onClick={(e)=>{
-             
-           setNodecheck(!nodeCheck)
-            }} className="btn btn-sm">Add</button>
+            <button
+              onClick={(e) => {
+                setNodecheck(!nodeCheck);
+              }}
+              className="btn btn-sm"
+            >
+              Add
+            </button>
           </li>
         </ul>
       </div>
@@ -86,161 +105,483 @@ React.useEffect(()=>{
   );
 }
 
-
 const MenuBar = ({ editor }) => {
+  const classes = styles;
+
   if (!editor) {
-    return null
+    return null;
   }
 
   return (
-    <>
+    <div className="flex gap-1 items-baseline flex-wrap">
       <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        className={editor.isActive('bold') ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleBold()
+            .run()
+        }
+        className={
+          editor.isActive("bold") ? classes.iconactive : classes.icondeactive
+        }
       >
-        bold
+        <FaBold />
       </button>
       <button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        className={editor.isActive('italic') ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleItalic()
+            .run()
+        }
+        className={
+          editor.isActive("italic") ? classes.iconactive : classes.icondeactive
+        }
       >
-        italic
+        <FaItalic />
       </button>
       <button
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        className={editor.isActive('strike') ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleStrike()
+            .run()
+        }
+        className={
+          editor.isActive("strike") ? classes.iconactive : classes.icondeactive
+        }
       >
-        strike
+        <MdStrikethroughS />
       </button>
+
+
+
       <button
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        className={editor.isActive('code') ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleSuperscript()
+            .run()
+        }
+        className={
+          editor.isActive("superscript") ? classes.iconactive : classes.icondeactive
+        }
+      >
+        Superscript
+      </button>
+
+      <button
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleSubscript()
+            .run()
+        }
+        className={
+          editor.isActive("subscript") ? classes.iconactive : classes.icondeactive
+        }
+      >
+        Subscript
+      </button>
+
+
+
+      <button
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleCode()
+            .run()
+        }
+        className={
+          editor.isActive("code") ? classes.iconactive : classes.icondeactive
+        }
       >
         code
       </button>
-      <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+      <button
+        className={classes.icondeactive}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .unsetAllMarks()
+            .run()
+        }
+      >
         clear marks
       </button>
-      <button onClick={() => editor.chain().focus().clearNodes().run()}>
+      <button
+        className={classes.icondeactive}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .clearNodes()
+            .run()
+        }
+      >
         clear nodes
       </button>
+
       <button
-        onClick={() => editor.chain().focus().setParagraph().run()}
-        className={editor.isActive('paragraph') ? 'is-active' : ''}
+        onClick={() =>
+          editor.chain().focus().setTextAlign('left').run()
+        }
+        className={
+          editor.isActive({ textAlign: 'left'} )
+          ? classes.iconactive
+          : classes.icondeactive
+          
+  
+        }
+      >
+        Left
+      </button>
+
+      <button
+        onClick={() =>
+          editor.chain().focus().setTextAlign('center').run()
+        }
+        className={
+          editor.isActive({ textAlign: 'center'} )
+          ? classes.iconactive
+          : classes.icondeactive
+          
+  
+        }
+      >
+        center
+      </button>
+
+
+
+
+      <button
+        onClick={() =>
+          editor.chain().focus().setTextAlign('right').run()
+        }
+        className={
+          editor.isActive({ textAlign: 'right'} )
+          ? classes.iconactive
+          : classes.icondeactive
+          
+  
+        }
+      >
+        right
+      </button>
+
+
+
+
+
+      <button
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .setParagraph()
+            .run()
+        }
+        className={
+          editor.isActive("paragraph")
+            ? classes.iconactive
+            : classes.icondeactive
+        }
       >
         paragraph
       </button>
       <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleHeading({ level: 1 })
+            .run()
+        }
+        className={
+          editor.isActive("heading", { level: 1 })
+            ? classes.iconactive
+            : classes.icondeactive
+        }
       >
         h1
       </button>
       <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleHeading({ level: 2 })
+            .run()
+        }
+        className={
+          editor.isActive("heading", { level: 2 })
+            ? classes.iconactive
+            : classes.icondeactive
+        }
       >
         h2
       </button>
       <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleHeading({ level: 3 })
+            .run()
+        }
+        className={
+          editor.isActive("heading", { level: 3 })
+            ? classes.iconactive
+            : classes.icondeactive
+        }
       >
         h3
       </button>
       <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-        className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleHeading({ level: 4 })
+            .run()
+        }
+        className={
+          editor.isActive("heading", { level: 4 })
+            ? classes.iconactive
+            : classes.icondeactive
+        }
       >
         h4
       </button>
       <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-        className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleHeading({ level: 5 })
+            .run()
+        }
+        className={
+          editor.isActive("heading", { level: 5 })
+            ? classes.iconactive
+            : classes.icondeactive
+        }
       >
         h5
       </button>
       <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-        className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleHeading({ level: 6 })
+            .run()
+        }
+        className={
+          editor.isActive("heading", { level: 6 })
+            ? classes.iconactive
+            : classes.icondeactive
+        }
       >
         h6
       </button>
       <button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive('bulletList') ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleBulletList()
+            .run()
+        }
+        className={
+          editor.isActive("bulletList")
+            ? classes.iconactive
+            : classes.icondeactive
+        }
       >
         bullet list
       </button>
       <button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive('orderedList') ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleOrderedList()
+            .run()
+        }
+        className={
+          editor.isActive("orderedList")
+            ? classes.iconactive
+            : classes.icondeactive
+        }
       >
         ordered list
       </button>
       <button
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive('codeBlock') ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleCodeBlock()
+            .run()
+        }
+        className={
+          editor.isActive("codeBlock")
+            ? classes.iconactive
+            : classes.icondeactive
+        }
       >
         code block
       </button>
       <button
-        onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={editor.isActive('blockquote') ? 'is-active' : ''}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .toggleBlockquote()
+            .run()
+        }
+        className={
+          editor.isActive("blockquote")
+            ? classes.iconactive
+            : classes.icondeactive
+        }
       >
         blockquote
       </button>
-      <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+      <button
+            className={classes.icondeactive}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .setHorizontalRule()
+            .run()
+        }
+      >
         horizontal rule
       </button>
-      <button onClick={() => editor.chain().focus().setHardBreak().run()}>
+      <button
+            className={classes.icondeactive}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .setHardBreak()
+            .run()
+        }
+      >
         hard break
       </button>
-      <button onClick={() => editor.chain().focus().undo().run()}>
+      <button
+            className={classes.icondeactive}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .undo()
+            .run()
+        }
+      >
         undo
       </button>
-      <button onClick={() => editor.chain().focus().redo().run()}>
+      <button
+            className={classes.icondeactive}
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .redo()
+            .run()
+        }
+      >
         redo
       </button>
-    </>
-  )
-}
 
 
+      <div className="dropdown dropdown-right">
+        <div tabindex="0"  className={classes.icondeactive}>
+          Image
+        </div>
+        <ul
+          tabindex="0"
+          className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <div className="form-control">
+              <input
+             
+                type="text"
+                placeholder="Image Source"
+                className="mb-1 input input-bordered"
+              />
+            </div>
+            <button
+              onClick={(e) => {
+                if (true) {
+                  editor.chain().focus().setImage({ src: "https://global-uploads.webflow.com/59deb588800ae30001ec19c9/5d0bc991c864410c5ad87931_logo-new.svg" }).run()
+                }
+              }}
+              className="btn btn-sm"
+            >
+              Add
+            </button>
+          </li>
+        </ul>
+      </div>
 
-
-
-
+    </div>
+  );
+};
 
 function Editors() {
   const editor = useEditor({
     extensions: [
       StarterKit,
+      BulletList,
+      Document,
+      Paragraph,
+      Text,
+      OrderedList,
+      ListItem,
+      Superscript,
+      Subscript,
+      Image,
+      Dropcursor,
+      TextAlign.configure({
+        types: ['heading', 'paragraph','image','img'],
+      }),
     ],
     editorProps: {
       attributes: {
         class:
-          " prose prose-sm sm:prose lg:prose-lg xl:prose-2xl border-none bg-white-400 overflow-y-auto   "
-      }
+          " prose prose-sm sm:prose lg:prose-lg xl:prose-2xl border-none bg-white overflow-y-auto   ",
+      },
     },
-    content: `
-      Take Your Notes here
-    `,
-  })
+
+  });
 
   return (
     <div className="w-full ">
-      <div className="m-1 p-2 shadow-xl bg-white-400">
-      <MenuBar editor={editor} />
+      <div className="m-1 h-auto p-1 shadow-xxl bg-gray-200">
+        <MenuBar editor={editor} />
       </div>
-      
 
-      <div className="m-1 shadow-md border-2 border-gray-700 "><EditorContent editor={editor}   /></div>
-      
+      <div className="m-1 shadow-md border-2 border-gray-700 ">
+        <EditorContent editor={editor} />
+      </div>
     </div>
-  )
+  );
 }
-
 
 export default function test() {
   return (
@@ -257,12 +598,21 @@ export default function test() {
               />
             </div>
           </div>
-        <div className="flex justify-between items-center"> 
-        <Nodes />
-        <button>THis</button>
-        
-        </div>
-    <Editors />
+          <div className="flex flex-col-2 justify-between items-center">
+            <Nodes />
+            <div class="m-2 flex items-start justify-center ">
+              <button onClick={(e) => {}} className="btn  btn-sm btn-circle">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-center items-baseline">
+            <div className="w-full">
+              <Editors />
+            </div>
+
+            <div className="w-1/6">Hello</div>
+          </div>
         </div>
       </div>
     </div>
