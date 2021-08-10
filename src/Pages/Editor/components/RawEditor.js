ca183c999ebeb9914a00469f673802cd20251148
maibrain <maibrainceo@gmail.com>
2021-08-10 16:01:10 +0530

    Before Removing Modules

--- src/Pages/Editor/components/RawEditor.js
+++ src/Pages/Editor/components/RawEditor.js
@@ -18,95 +18,104 @@ import Superscript from "@tiptap/extension-superscript";
 import Image from "@tiptap/extension-image";
 import Dropcursor from "@tiptap/extension-dropcursor";
 import TextAlign from "@tiptap/extension-text-align";
-
 import MenuBar from "./Menubar";
-
-
 
 import "./editor.css";
 
 const styles = {
-    iconactive: "mx-1 px-2 py-2  bg-gray-400 shadow btn btn-ghost",
-    icondeactive: "mx-1 px-2 py-2 bg-gray-100 shadow btn btn-ghost",
-    ol: "list-inside",
-  };
+  iconactive: "mx-1 px-2 py-2  bg-gray-400 shadow btn btn-ghost",
+  icondeactive: "mx-1 px-2 py-2 bg-gray-100 shadow btn btn-ghost",
+  ol: "list-inside",
+};
 function RawEditor(props) {
-    const editor = useEditor({
-      extensions: [
-        StarterKit,
-        BulletList,
-        Document,
-        Paragraph,
-        Text,
-        OrderedList,
-        ListItem,
-        Superscript,
-        Subscript,
-        Image,
-  
-        TextAlign.configure({
-          types: ["heading", "paragraph", "image", "img"],
-        }),
-      ],
-      editorProps: {
-        attributes: {
-          class:
-            "prose prose-sm sm:prose lg:prose-lg xl:prose-2xl border-none bg-white overflow-y-auto   ",
-        },
-      },
-    });
-  
-    return (
-      <div className="w-full">
-        {editor && (
-          <BubbleMenu
-            className="flex justify-baseline items-center"
-            tippyOptions={{
-              placement: "bottom",
-  
-              // if you want
-            }}
-            editor={editor}
-          >
-            <button onClick={props.noteFunc} className={styles.icondeactive}>
-              Take Note
-            </button>
-            <button
-              onClick={() =>
-                editor
-                  .chain()
-                  .focus()
-                  .toggleItalic()
-                  .run()
-              }
-              className={editor.isActive("italic") ? "is-active" : ""}
-            >
-              italic
-            </button>
-            <button
-              onClick={() =>
-                editor
-                  .chain()
-                  .focus()
-                  .toggleStrike()
-                  .run()
-              }
-              className={editor.isActive("strike") ? "is-active" : ""}
-            >
-              strike
-            </button>
-          </BubbleMenu>
-        )}
-        <div className="m-1 h-auto p-1 shadow-xxl bg-gray-200">
-          <MenuBar editor={editor} />
-        </div>
-  
-        <div className="m-1 shadow-md border-2 border-gray-700 ">
-          <EditorContent editor={editor} />
-        </div>
-      </div>
-    );
-  }
-  
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      BulletList,
+      Document,
+      Paragraph,
+      Text,
+      OrderedList,
+      ListItem,
+      Superscript,
+      Subscript,
+      Image,
 
-  export default RawEditor;
+      TextAlign.configure({
+        types: ["heading", "paragraph", "image", "img"],
+      }),
+     
+    ],
+    editorProps: {
+      attributes: {
+        class:
+          "prose prose-sm sm:prose lg:prose-lg xl:prose-2xl border-none bg-white overflow-y-auto   ",
+      },
+    },
+  });
+
+  return (
+    <div className="w-full">
+      {editor && <BubbleMenus editor={editor} noteFunc={props.noteFunc} />}
+      <div className="m-1 h-auto p-1 shadow-xxl bg-gray-200">
+        <MenuBar editor={editor} />
+      </div>
+
+      <div className="m-1 shadow-md border-2 border-gray-700 ">
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  );
+}
+
+function BubbleMenus(props) {
+  return (
+    <BubbleMenu
+      className="flex justify-baseline items-center"
+      tippyOptions={{
+        placement: "bottom",
+
+        // if you want
+      }}
+      editor={props.editor}
+    >
+      <button onClick={props.noteFunc} className={styles.icondeactive}>
+        Take Note
+      </button>
+
+      <button onClick={()=>{
+props.editor.chain().focus().toggleHighlight({
+  color: 'red'
+}).run()
+      }} className={styles.icondeactive}>
+        Highlight
+      </button>
+      <button
+        onClick={() =>
+          props.editor
+            .chain()
+            .focus()
+            .toggleItalic()
+            .run()
+        }
+        className={props.editor.isActive("italic") ? "is-active" : ""}
+      >
+        italic
+      </button>
+      <button
+        onClick={() =>
+          props.editor
+            .chain()
+            .focus()
+            .toggleStrike()
+            .run()
+        }
+        className={props.editor.isActive("strike") ? "is-active" : ""}
+      >
+        strike
+      </button>
+    </BubbleMenu>
+  );
+}
+
+export default RawEditor;
